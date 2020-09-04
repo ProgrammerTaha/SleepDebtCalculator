283685c4ebe1e9a076112cc1f1c91acf156e68c9
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 15:45:30 +0300

    comments

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 /*
 
-test test test
+todo: calculate if you’re getting enough sleep each week
+
+todo: determine the actual and ideal hours of sleep for each night of the last week
+
+todo: calculate, in hours, how far you are from your weekly sleep goal
 
 */
+
