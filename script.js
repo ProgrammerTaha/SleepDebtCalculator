2b70d28269d248b3ff196508f63b2f6ad4cbce18
programmertaha <programmer.taha.1y@gmail.com>
2020-09-04 16:04:12 +0300

    1# task

--- script.js
+++ script.js
@@ -8,3 +8,24 @@ todo: calculate, in hours, how far you are from your weekly sleep goal
 
 */
 
+//determining how many hours of sleep you got each night of the week
+const getSleepHours = day => {
+  day = day.toLowerCase();
+  switch (day) {
+    case 'sunday':
+      return 8;
+    case 'monday':
+      return 7;
+    case 'tuesday':
+      return 8;
+    case 'wednesday':
+      return 5;
+    case 'thursday':
+      return 7;
+    case 'friday':
+      return 6;
+    case 'saturday':
+      return 6;
+  }
+};
+
